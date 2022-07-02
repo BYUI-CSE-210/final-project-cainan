3fa790c7c64d30b2859732fcdf909ade64b5e3f9
EvanWampler <etwampler@byui.edu>
2022-07-02 19:16:41 -0400

    small fixes to yeti class

--- yeti/game/entities/yetiClass.py
+++ yeti/game/entities/yetiClass.py
@@ -1,29 +1,31 @@
-from yeti.game.entities.entity import Entity
-from yeti.game.shared.point import Point
+from game.entities.entity import Entity
+from game.shared.point import Point
 import pyray as pr
 from pyray import Vector2
+from game.services.keyboard_service import KeyboardService
+from game.services.audio_service import AudioService
 
 
 
 class Yeti(Entity):
     def __init__(self,_keyboard_service,_audio_service) -> None:
-        super().__init__(_keyboard_service,_audio_service)
+        super().__init__()
+        self.yetiSprite = pr.load_texture("game/entities/images/yeti.png")
         self.center = Point()
         self.x = self.center.x
         self.y = self.center.y
-        self.frameHeight = float
-        self.frameWidth = float
+        self.timer = 0.0
+        self.frame = int()
+        self.frameWidth = int(self.yetiSprite.width/8)
+        self.frameHeight = self.yetiSprite.height/6
+        self.maxFrames = 8
         self.position = Vector2()
         self.rectangle = pr.Rectangle()
 
     def draw(self):
-        self.yetiSprite = pr.load_texture("yeti/game/entities/images/yeti.png")
-        self.frameWidth = float(self.yetiSprite/8)
-        self.frameHeight = float(self.yetiSprite/6)
-        self.maxFrames = int(self.yetiSprite.width/self.frameWidth)
         self.position = Vector2(self.x,self.y)
         self.rectangle = pr.Rectangle(self.frameWidth * self.frame,0,self.frameWidth,self.frameHeight)
-        self.picFrame = pr.draw_texture_rec(self.yetiSprite,self.rectangle,self.position,pr.BLUE)
+        self.picFrame = pr.draw_texture_rec(self.yetiSprite,self.rectangle,self.position,pr.WHITE)
 
     def advance(self):
         self.timer = float(0.0)
@@ -33,9 +35,12 @@ if __name__ == "__main__":
 
     pr.init_window(900,750,"YetiQuest")
     pr.set_target_fps(60)
+    _aud_serv = AudioService()
+    _ks = KeyboardService()
+
 
     while not pr.window_should_close():
-        yeti = Yeti()
+        yeti = Yeti(_ks,_aud_serv)
         pr.begin_drawing()
         pr.clear_background(pr.RAYWHITE)
 
@@ -44,6 +49,7 @@ if __name__ == "__main__":
         if yeti.timer >= 0.2:
             yeti.timer = 0.0
             yeti.frame += 1
+            print(f"frame: {yeti.frame}")
         
         yeti.frame = yeti.frame % yeti.maxFrames 
 
