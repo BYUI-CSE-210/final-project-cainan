dfb7ca8a30f7614bcf3ff93f9ad4d858e087e811
EvanWampler <etwampler@byui.edu>
2022-07-14 18:17:14 -0400

    boss axe fixes

--- yeti/game/deeds/boss_create_axe_deed.py
+++ yeti/game/deeds/boss_create_axe_deed.py
@@ -1,5 +1,7 @@
 from game.deeds.deed import Deed
 from game.entities.axe import Axe
+import pyray as pr
+from random import randint
 
 class BossCreateAxeDeed(Deed):
     def __init__(self, boss ,axes:list, service_manager=None, debug=False) -> None:
@@ -8,7 +10,9 @@ class BossCreateAxeDeed(Deed):
         self.boss = boss
 
     def execute(self):
-        axe = Axe(self.service_manager,self.boss.position,-1)
+        rand_num = randint(1,3)
+        for i in range(3):
+            axe = Axe(self.service_manager,pr.Vector2(self.boss.position.x, self.boss.position.y + self.boss.frameHeight/rand_num),-1)
         self.axes.append(axe)
         if self._debug:
             print(self.axes)
