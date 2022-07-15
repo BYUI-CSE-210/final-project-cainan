cfd18bd7ca12e2eca9424c2a39a36a56fabdf92b
Koomba22 <kes19002@byui.edu>
2022-07-15 11:09:39 -0600

    update collision for orange slime ammo

--- yeti/game/deeds/player_detect_enemy_collisions_deed.py
+++ yeti/game/deeds/player_detect_enemy_collisions_deed.py
@@ -2,6 +2,7 @@ import pyray as pr
 from game.deeds.deed import Deed
 from game.entities.entity import Entity
 from game.entities.yeti import Yeti
+from yeti.game.entities.orange_slime import OrangeSlime
 
 class PlayerDetectEnemyCollisionsDeed(Deed):
     def __init__(self, player, axes, enemies, birds, slimes, service_manager=None, debug=False) -> None:
@@ -15,6 +16,10 @@ class PlayerDetectEnemyCollisionsDeed(Deed):
         for entity_list in self._entities:
             for entity in entity_list:
                 if pr.check_collision_recs(self._player.get_hitbox(), entity.get_hitbox()):
-                    self._player.got_hit()
-                    entity.got_hit()
+                    if isinstance(entity, OrangeSlime):
+                        self._player.add_ammo()
+                    else:
+                        self._player.got_hit()
+                        entity.got_hit()
                     entity_list.remove(entity)
+                
